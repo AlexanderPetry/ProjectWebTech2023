f35c42efcae881f4183f742c3fb7100b65424409
YPTF-Engineering\AlexanderPetry <alexanderpetry2@gmail.com>
2023-05-29 12:21:48 +0200

    centered and cleaned up database data

--- Logic.php
+++ Logic.php
@@ -18,10 +18,10 @@
 
     $data = pg_fetch_all($result);
 
-    echo "<table>";
-    echo "<tr><th>number</th><th>bool</th></tr>";
+    echo "<table style='margin: 0 auto; border-collapse: collapse;'>";
+    echo "<tr><th style='border: 1px solid black; padding: 5px;'>number</th><th style='border: 1px solid black; padding: 5px;'>bool</th></tr>";
     foreach ($data as $row) {
-        echo "<tr><td>".$row['number']."</td><td>".$row['bool']."</td></tr>";
+        echo "<tr><td style='border: 1px solid black; padding: 5px; text-align: center;'>".$row['number']."</td><td style='border: 1px solid black; padding: 5px; text-align: center;'>".$row['bool']."</td></tr>";
     }
     echo "</table>";
 ?>
