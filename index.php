b23016ee5a2da136537df41746fb3a94a09e8f9e
YPTF-Engineering\AlexanderPetry <alexanderpetry2@gmail.com>
2023-05-29 16:21:04 +0200

    fixed button try 1

--- index.php
+++ index.php
@@ -72,8 +72,6 @@
 
     <div id="content3" style="display: none;">
         <h2>Content 3</h2>
-        
-        <button id="downloadButton">Download</button>
 
         <p id="preview"> data preview </p>
 
@@ -81,9 +79,9 @@
             var dataFromPHP = <?php echo $encodedData; ?>;
         </script>
 
-
-        <script src="DownloadLogic.js"></script>
-
+        <button id="downloadButton">Download
+          <script src="DownloadLogic.js"></script>
+        </button>
 
     </div>
 </body>
