f333290b338eeb83a901f635c21ff3241889757a
YPTF-Engineering\AlexanderPetry <alexanderpetry2@gmail.com>
2023-05-30 12:59:02 +0200

    added correct data

--- DownloadLogic.js
+++ DownloadLogic.js
@@ -1,7 +1,18 @@
-function downloadData() {
-    // Generate the data to be downloaded
-    var data = "This is the data to be downloaded.";
+// get data
+var encodedData = <?php echo $encodedData; ?>;
+console.log(encodedData);
 
+// set variables
+var data = "";
+var filetype = ".txt"
+var filename = "DownloadFile"
+
+//
+data = encodedData;
+
+
+//download data
+function downloadData() {
     // Create a Blob object from the data
     var blob = new Blob([data], { type: "text/plain" });
 
@@ -11,7 +22,7 @@ function downloadData() {
     // Create a temporary <a> element to initiate the download
     var link = document.createElement("a");
     link.href = url;
-    link.download = "data.txt"; // Set the desired file name
+    link.download = filename + filetype; // Set the desired file name
 
     // Append the <a> element to the document body
     document.body.appendChild(link);
