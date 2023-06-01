9d26935e1aad52b1c13c1f31d2105956450726e8
YPTF-Engineering\AlexanderPetry <alexanderpetry2@gmail.com>
2023-06-01 20:32:33 +0200

    patched possible vulnerablilities

--- api/v1/temperature.php
+++ api/v1/temperature.php
@@ -2,11 +2,7 @@
 $body = file_get_contents("php://input");
 
 $data = json_decode($body, true);
-$data = (string) $data['temperature'];
-
-echo "Temperature: ";
-echo $data;
-echo "\n";
+$temperature = isset($data['temperature']) ? $data['temperature'] : null;
 
 function runPythonScript() {
     $pythonScript = "/path/to/your/python_script.py";
@@ -23,8 +19,9 @@ function runPythonScript() {
     }
 }
 
-if ($data) {
+if ($temperature) {
     runPythonScript();
+
     // Set connection parameters
     $host       = "localhost";
     $port       = "5432"; 
@@ -39,30 +36,22 @@ if ($data) {
     // Get current timestamp
     $time = date('Y-m-d H:i:s');
 
-    echo "Time: ";
-    echo $time;
-    echo "\n";
+    // Validate temperature value (example: numeric between -100 and 100)
+    if (!is_numeric($temperature) || $temperature < -100 || $temperature > 100) {
+        echo "Invalid temperature value.";
+        exit;
+    }
 
+    // Sanitize temperature value
+    $temperature = pg_escape_string($dbconn, $temperature);
 
-    $insertQuery = "INSERT INTO $table (time, temperature) VALUES ('{$time}', '{$data}')";
-    
+    $insertQuery = "INSERT INTO $table (time, temperature) VALUES ($1, $2)";
 
-    echo "insert query: ";
-    echo $insertQuery;
-    echo "\n";
+    // Prepare the statement
+    $stmt = pg_prepare($dbconn, "insert_query", $insertQuery);
 
-    // Remove trailing comma and space
-    $insertQuery = rtrim($insertQuery, ', ');
-
-    echo "insert query: ";
-    echo $insertQuery;
-    echo "\n";
-
-    $result = pg_query($dbconn, $insertQuery);
-
-    echo "result: ";
-    echo $result;
-    echo "\n";
+    // Execute the statement with the data as parameters
+    $result = pg_execute($dbconn, "insert_query", array($time, $temperature));
 
     if ($result) {
         echo "Data inserted into the PostgreSQL database.\n";
